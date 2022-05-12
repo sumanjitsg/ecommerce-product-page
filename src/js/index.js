f209b5717c391873497c3fe9453d077d26b72e04
Sumanjit Sengupta <sumanjitsengupta08@gmail.com>
2022-05-12 20:23:38 +0530

    modal navigation added

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,29 @@
 import '../index.html';
 import '../sass/styles.scss';
+
+const openMenu = document.getElementById('open-menu');
+const closeMenu = document.getElementById('close-menu');
+const modal = document.getElementById('modal');
+const nav = document.getElementById('nav');
+
+openMenu.addEventListener('click', () => {
+  modal.classList.remove('modal--closed');
+  nav.classList.remove('modal--closed__nav');
+
+  modal.classList.add('modal--open');
+  nav.classList.add('modal--open__nav');
+});
+
+closeMenu.addEventListener('click', () => {
+  modal.classList.replace(
+    'modal--open', 'modal--closed'
+  );
+  nav.classList.replace(
+    'modal--open__nav', 'modal--closed__nav'
+  );
+});
+
+modal.addEventListener('animationend', (e) => {
+  modal.classList.remove('modal--closed');
+  nav.classList.remove('modal--closed__nav');
+});
